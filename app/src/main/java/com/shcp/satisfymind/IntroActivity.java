4f80dde1a67bc182aa10a33d98bed7a745deafb4
Seong Hyun <tjd88012@naver.com>
2020-07-27 09:20:06 +0900

    Intro

--- app/src/main/java/com/shcp/satisfymind/IntroActivity.java
+++ app/src/main/java/com/shcp/satisfymind/IntroActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
