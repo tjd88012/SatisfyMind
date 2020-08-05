269032870b5b1adce4da9da205a16a775fdb34b0
Seong Hyun <tjd88012@naver.com>
2020-08-05 16:41:32 +0900

    FSListFragment

--- app/src/main/java/com/shcp/satisfymind/FSListAdapter.java
+++ app/src/main/java/com/shcp/satisfymind/FSListAdapter.java
@@ -1,10 +1,12 @@
 package com.shcp.satisfymind;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -58,6 +60,14 @@ public class FSListAdapter extends RecyclerView.Adapter {
             title=itemView.findViewById(R.id.fs_list_title);
             countFavor=itemView.findViewById(R.id.fs_list_count);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FSListItem listItem=fsItem.get(getLayoutPosition());
+                    Intent intent=new Intent(context, FSListItemActivity.class);
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
